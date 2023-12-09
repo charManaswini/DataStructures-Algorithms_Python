80dead85393784c1196bedbeef707f49386c63c3
charManaswini <manupashad99@gmail.com>
2023-12-10 01:32:17 +0530

    day1

--- Day_1.py
+++ Day_1.py
@@ -1,1 +1,8 @@
-print("hello world!")
+from array import array 
+import array as arr
+a = arr.array('i',[1,2,3])
+print("The new created array is:")
+for i in range(0, 3):
+    print(a[i], end=" ")
+print()
+
